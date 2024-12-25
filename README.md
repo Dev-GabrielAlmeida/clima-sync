3aa09f24830a3d598a4bbc37521dc85507e0a7ad
Gabriel Silva de Almeida <131467393+Dev-GabrielAlmeida@users.noreply.github.com>
2024-12-25 14:59:54 -0300

    Subindo o projeto

---
+++ README.md
@@ -0,0 +1,41 @@
+# Clima Sync
+
+![Clima Sync](https://www.dropbox.com/scl/fi/yy58aolhd9ied6f800h92/project-climasync.png?rlkey=ar5aummygaxvmv5bqgdsqssf9&st=iu4enpa6&dl=0)
+
+## Descrição
+
+O **Clima Sync** permite que o usuário receba dados climáticos, como temperatura, umidade, velocidade do vento e uma descrição do clima, de qualquer cidade do mundo, utilizando apenas o nome da cidade. O projeto está conectado à API OpenWeather para fornecer informações atualizadas sobre o clima.
+
+## Tecnologias Usadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Instalação
+
+Para rodar o projeto, você precisará ter o [Visual Studio Code](https://code.visualstudio.com/) e o [Node.js](https://nodejs.org/) instalados em sua máquina. Siga os passos abaixo:
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/clima-sync.git
+
+Depois abra o projeto no Visual Studio Code e iniciar o servidor local. Para isso, você pode usar a extensão Live Server ou um comando específico do Node.js, dependendo da sua configuração.
+
+    1.Abra o arquivo index.html no seu navegador.
+    2. Insira o nome da cidade desejada e clique em "Buscar".
+    3. Os dados climáticos aparecerão na tela.
+
+## Contribuições
+
+Contribuições são bem-vindas! Se você deseja contribuir para o Clima Sync, siga os passos abaixo:
+
+1 - Fork este repositório.
+2 - Crie uma nova branch.
+3 - Faça suas alterações e commit.
+4 - Envie para o repositório remoto.
+5 - Abra um Pull Request.
+
+## Contato
+
+Se você tiver alguma dúvida ou feedback, sinta-se à vontade para entrar em contato comigo pelo meu LinkedIn.
---
+++ src/scripts/services/apiConnector.js
@@ -0,0 +1,80 @@
+const weatherApiKey = "885c0cf1895ad0adb880824d152d27b6";
+
+const getData = async (city) => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApiKey}&lang=enr&units=metric`)
+    const dataReceived = await response.json()
+
+    return dataReceived
+}
+
+const fetchWeatherData = async (city) => {
+
+    try {
+        const apiResponse = await getData(city);
+
+        // Verifica se a resposta da API é válida
+        if (!apiResponse || apiResponse.cod !== 200) {
+            throw new Error("Cidade não encontrada ou erro na API");
+        }
+
+        // Aguardando a bandeira carregar
+        const flagElement = await getFlag(apiResponse.sys.country);
+
+        // Obtem o código do ícone do clima
+        const weatherIconCode = apiResponse.weather[0].icon;
+        const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}@2x.png`;
+
+        const weatherInfo = {
+            temperature: `${parseInt(apiResponse.main.temp)} °C`,
+            location: `<i class="bi bi-geo-alt-fill"></i> ${apiResponse.name}, ${flagElement.outerHTML}`,
+            descriptionWeather: apiResponse.weather[0].description,
+            humidity: `${apiResponse.main.humidity}<span>%</span>`,
+            wind: `${apiResponse.wind.speed}<span>km/h</span>`,
+            icon: `<img src="${weatherIconUrl}" alt="Ícone do clima" width="50" height="50">`,
+        };
+
+        // Chamando a função para atualizar os dados climaticos
+        updateWeatherDisplay(weatherInfo);
+
+    } catch (error) {
+
+        console.error("Erro ao buscar dados do clima:", error);
+        return `Erro: ${error.message}`;
+
+    }
+}
+
+// Cidade padrão inicial
+fetchWeatherData('London');
+
+// Adiciona um ouvinte ao botão de busca
+document.getElementById('btnSearchWeather').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputCity = document.getElementById('inputCity');
+    const city = inputCity.value.trim();
+
+    fetchWeatherData(city);
+    cityInput.value = '';
+});
+
+// Função para atualizar as informações do clima
+const updateWeatherDisplay = (weatherInfo) => {
+    Object.keys(weatherInfo).forEach((key) => {
+        document.getElementById(key).innerHTML = weatherInfo[key];
+    });
+}
+
+// Função para buscar a bandeira do local
+const getFlag = async (country) => {
+
+    const flagUrl = `https://flagsapi.com/${country}/flat/64.png`;
+
+    // Cria um elemento de imagem
+    const imgElement = document.createElement('img');
+    imgElement.src = flagUrl;
+    imgElement.width = 29;
+    imgElement.height = 29;
+
+    return imgElement; // Retorna o elemento de imagem
+}
